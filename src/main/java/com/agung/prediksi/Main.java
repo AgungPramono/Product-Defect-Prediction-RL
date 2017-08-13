e65bd0c03df5f4b8e5426f8a517fd18b597b0a4d
agungpramono <agungpermadi13@gmail.com>
2017-08-13 09:51:33 +0700

    hapus isi method main

--- src/main/java/com/agung/prediksi/Main.java
+++ src/main/java/com/agung/prediksi/Main.java
@@ -8,31 +8,12 @@
 
 package com.agung.prediksi;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.function.Consumer;
-
 /**
  *
  * @author agung
  */
 public class Main {
     public static void main(String[]args){
-        List<Integer> hasil = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            hasil.add(i);
-        }
         
-        //sebelum java 5
-        Iterator<Integer> iterator = hasil.iterator();
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            System.out.println(next);
-        }
-        
-        //sejak java 5
-        hasil.forEach(System.out::println);
     }
-
 }
